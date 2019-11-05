a5766754bc5d3f7ba4e6f6f7c91e5530958db94f
Ivliev Maxim <muss123balakovo@gmail.com>
2019-11-05 22:46:42 +0300

    Divide getWeather and render functions. Put them in a separate file

---
+++ Lab1/js/script.js
@@ -0,0 +1,42 @@
+function render(data) {
+    var htmlWeather = document.getElementById("WeatherInfo")
+    var htmlCity = document.getElementById("City")
+    if (data['cod'] === 200) {
+        //console.log("yes")
+        htmlCity.textContent = data["name"]
+        var template = "{{~it.weather :value:index}}<li>{{=value.parametr}} - {{=value.value}}</li>{{~}}"
+        var json = {
+            "weather": [
+                { "parametr": "weather", "value": data["weather"][0]["description"] },
+                { "parametr": "temperature (Celsius)", "value": data["main"]["temp"] },
+                { "parametr": "humidity (%)", "value": data["main"]["humidity"] },
+                { "parametr": "pressure (hPa)", "value": data["main"]["pressure"] },
+                { "parametr": "wind speed (meter/sec)", "value": data["wind"]["speed"] }
+            ]
+        }
+        var tempFn = doT.template(template)
+        htmlWeather.innerHTML = tempFn(json)
+    } else {
+        //console.log("no")
+        htmlCity.textContent = data["message"]
+        htmlWeather.innerHTML = ""
+    }
+}
+
+function getweather(city) {
+    var request = new XMLHttpRequest()
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q='.concat(city, "&APPID=f8e5ebb3f762d1a58aaff7f643d7410b&units=metric"), true)
+    request.onload = function () {
+        data = JSON.parse(this.response)
+        //console.log(data)
+        render(data)
+    }
+    request.send()
+}
+
+function action() {
+    var city = document.getElementById("_citysearch").value;
+    //console.log(city)
+    getweather(city)
+
+}
